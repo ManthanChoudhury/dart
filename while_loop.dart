0e4410b1e47215c0e3ffabaae3f2b857d571697a
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2020-07-05 18:13:05 +0530

    Create while_loop.dart

---
+++ while_loop.dart
@@ -0,0 +1,34 @@
+void main(){
+ /* int a=1;  // same as c++
+  while (a<=10){
+    print(a);
+    a++;
+  }*/
+  
+  int i =1;
+  while (i<=10){
+    if(i%2==0){
+      print(i);
+    }
+    i++;
+}
+}
+/* result
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+2
+4
+6
+8
+10
+
+*/
