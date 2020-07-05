febe4987dece92d7444ebbed2d95a3b4529fc4e1
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2020-07-05 17:41:30 +0530

    Create switch_io.dart

---
+++ switch_io.dart
@@ -0,0 +1,43 @@
+void main(){
+ // print("enter your choice");
+  
+  //int i = int.parse(stdin.readLineSync());
+  
+ // print("Your favourite number is $i /n");
+  
+  // dartpad dont support input output operation
+  
+  int i = 7;
+  switch(i){
+    case 1:
+      print("one $i");
+      break;
+    case 2:
+      print("Two $i");
+      break;
+    case 3:
+      print("three $i");
+      break;
+    case 4:
+      print("four $i");
+      break;
+    case 5:
+      print("five $i");
+      break;
+    case 6:
+      print("six $i");
+      break;
+    case 7:
+      print("seven $i");
+      break;
+    case 8:
+      print("eight $i");
+      break;
+    default:
+      print("invalid $i");
+  }
+}
+
+
+//result
+//seven 7
