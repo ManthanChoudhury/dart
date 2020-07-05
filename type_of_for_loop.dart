0a931178f1b2f0e86c2511d80d0fd7cb1e76e8fc
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2020-07-05 17:53:59 +0530

    Create type_of_for_loop.dart

---
+++ type_of_for_loop.dart
@@ -0,0 +1,63 @@
+void main(){
+  
+  //simple for loop
+  for(int i= 1; i<=10 ;i++){
+    print("result $i \n");
+  }
+  
+   for(int i= 1; i<=10 ;i++){
+     if(i % 2 == 0)
+    print("result $i \n");
+  }
+  // for in for loop 
+  // in python like
+  List language = ["C++","C","DART","PYTHON","HTML","CSS","LINUX","TERRAFORM","KUBERNETS","openstack","RISING android developer"];
+  
+for(String langs in language){
+  print("languages I know $langs");
+}  
+}
+
+/*result 
+result 1 
+
+result 2 
+
+result 3 
+
+result 4 
+
+result 5 
+
+result 6 
+
+result 7 
+
+result 8 
+
+result 9 
+
+result 10 
+
+result 2 
+
+result 4 
+
+result 6 
+
+result 8 
+
+result 10 
+
+languages I know C++
+languages I know C
+languages I know DART
+languages I know PYTHON
+languages I know HTML
+languages I know CSS
+languages I know LINUX
+languages I know TERRAFORM
+languages I know KUBERNETS
+languages I know openstack
+languages I know RISING android developer
+*/
