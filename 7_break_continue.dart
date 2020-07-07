ba4e17136289bfb6635434b914cccc895c0b9419
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2020-07-07 05:30:34 +0530

    Create 7_break_continue.dart

---
+++ 7_break_continue.dart
@@ -0,0 +1,32 @@
+void main(){
+  
+  /*for(var i=1;i<10;i++){
+    
+    print(i);
+    
+    if(i==5){
+      break;  
+      // remember break always work on nearer loop
+      // it will take out u from loop
+    }
+     
+    print("the condition is working  ");
+  }
+  print("the condition breaked");*/
+  
+  //continue keyword
+  
+  for(int i=1;i<=10;i++){
+    
+    print(i);
+    
+    if(i == 5){
+      continue ;  
+      // remember break always work on nearer loop
+      // it will take out u from loop
+    }
+  }
+
+  
+}
+// still some error 
