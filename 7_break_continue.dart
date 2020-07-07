b22f52617d3a51141db73f9791bbd9da148f940e
MANTHAN CHOUDHURY <45136716+ManthanChoudhury@users.noreply.github.com>
2020-07-07 06:00:02 +0530

    Update 7_break_continue.dart

--- 7_break_continue.dart
+++ 7_break_continue.dart
@@ -1,6 +1,6 @@
 void main(){
   
-  /*for(var i=1;i<10;i++){
+  for(var i=1;i<10;i++){
     
     print(i);
     
@@ -12,21 +12,46 @@ void main(){
      
     print("the condition is working  ");
   }
-  print("the condition breaked");*/
+  print("the condition breaked");
   
   //continue keyword
   
+  print(" ");
+  
   for(int i=1;i<=10;i++){
-    
-    print(i);
-    
     if(i == 5){
+      print("conditon breakdue to continue $i");
       continue ;  
-      // remember break always work on nearer loop
-      // it will take out u from loop
+      // remember continue always work on nearer loop
+      // it will skip one itration of loop and continue back in loop
+     
     }
+    print(i);
   }
 
   
 }
-// still some error 
+
+/* result
+1
+the condition is working  
+2
+the condition is working  
+3
+the condition is working  
+4
+the condition is working  
+5
+the condition breaked
+ 
+1
+2
+3
+4
+conditon breakdue to continue 5
+6
+7
+8
+9
+10
+*/
